a1aa818257aa78eeb27f17b504a6c1fdda064583
Chucho54 <jesusserr6482@gmail.com>
2025-03-10 00:32:42 -0600

    Segunda version

---
+++ pagina2-1.php
@@ -0,0 +1,33 @@
+<?php
+        function suma($a,$b){
+        return $a+$b;
+}
+function suma($d,$e){
+        return $d+$e;
+}
+function dividie($a,$b,$d,$e){
+        $resultadosum0=suma($a,$b);
+        $resultadosuma1=suma($c,$e);
+        return $resultadosum0/$resultadosuma1;
+}
+
+
+#codigo que se ejecuta inmediatamente
+$a = pow(4,2)/2 ;
+$b = 1/sqrt(4);
+$c = 3;
+$e = 1/2*pow(4,3);
+$d = dividie($a,$b,$d,$e);
+$f = sqrt($c);
+        ?>
+
+        <html>
+        <head>
+            <title>Funciones</title>
+    </head>
+    <body>
+        <p> <?php echo $f; ?> </p>
+      </body>
+    </html>
+
+
