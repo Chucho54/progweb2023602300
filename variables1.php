f463cd6778b53e9a5092a1bef96b0916df7c6c9a
Chucho54 <jesusserr6482@gmail.com>
2025-03-11 00:59:10 -0600

    Tarea Expresiones Version Final

--- variables1.php
+++ variables1.php
@@ -11,27 +11,26 @@ $a=($x+$y)/3;
 $b=suma0($divis,$a);
 $j= $b+(2*($x/$y));
 
-function div($numerador,$denominador){
-return $numerador/$denominador;
-}
+
 
 $x0=3;
 $numerador=(0.5*$x0)+((3+$x0)/2)*(2*pow($x0,2));
 $denominador=(2+3)*$x0;
-$h=div($numerador,$denominador);
+$h= $numerador/$denominador;
 
-function suma3($dd,$jp00){
-    return $dd+$jp00;
-}
-$v=pow((2*$x),2)+pow(3,2);
-$dd=sqrt($v)/5;
+
+
+
+$v=pow((2*$x),2);
+$vv=$v+pow(3,2);
+$dd=sqrt($vv)/5;
 $jp00=sqrt(pow($x,2));
-$ft=suma3($dd,$jp00);
+$ft=$dd+$jp00;
 
 
 
-$fraccik=((1/2)+(1/4)+(1/8));
-$frad= $fraccik*(pow($x,1/3));
+$fraccik=(0.5+0.25+0.125);
+$frad= $fraccik*($x**(1/3));
 $fracionarraiz=(sqrt($x)/2)+((3*pow($x,2))/4);
 $inzinkkd=$frad/$fracionarraiz;
 
@@ -41,14 +40,14 @@ $res=sqrt($sud/$sufr);
         ?>
 
         <html>
-        <head>
+        <head> EXPRESIONES
             <title> Expresiones </title>
     </head>
     <body>
-        <p> <img src="incisoa.jpeg" alt="Imagen" width="280" height="70" style="vertical-align: middle;"> = <?php echo  $j; ?> </p>
-        <p> <img src="incisob.jpeg" alt="Imagen" width="280" height="150" style="vertical-align: middle;"> = <?php echo $h; ?> </p>
-        <p> <img src="incisoc.jpeg" alt="Imagen" width="280" height="150" style="vertical-align: middle;"> = <?php echo $ft; ?> </p>
-        <p> <img src="incisod.jpeg" alt="Imagen" width="280" height="150" style="vertical-align: middle;"> = <?php echo number_format($inzinkkd,3); ?> </p>
-        <p> <img src="incisoe.jpeg" alt="Imagen" width="270" height="130" style="vertical-align: middle;"> = <?php echo number_format($res,4); ?> </p>
+        <p> <img src="incisoa.jpeg" alt="Imagen" width="280" height="70" style="vertical-align: middle;"> = <?php echo  $j; ?>    
+        <img src="incisob.jpeg" alt="Imagen" width="280" height="150" style="vertical-align: middle;"> = <?php echo $h; ?> 
+        <img src="incisoc.jpeg" alt="Imagen" width="280" height="150" style="vertical-align: middle;"> = <?php echo $ft; ?> </p>
+        <p> <img src="incisod.jpeg" alt="Imagen" width="280" height="150" style="vertical-align: middle;"> = <?php echo $inzinkkd; ?>
+        <img src="incisoe.jpeg" alt="Imagen" width="270" height="130" style="vertical-align: middle;"> = <?php echo $res; ?> </p>
       </body>
     </html>
